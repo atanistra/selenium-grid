805074a26d63a338df94baae9e202740a307cb72
atanistra <artur.tanistra@gmail.com>
2016-02-03 15:27:07 +0100

    add hub config

---
+++ entry_point.sh
@@ -0,0 +1,9 @@
+
+ROOT=/usr/selenium
+CONF=$ROOT/config.json
+
+$ROOT/generate_config.sh > $CONF
+echo "starting selenium hub with configuration:"
+cat $CONF
+
+java -jar /usr/selenium/selenium-server-standalone.jar -role hub -hubConfig $CONF
