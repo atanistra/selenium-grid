dc241a3d3aed788efe358748144b8d2cee229251
atanistra <artur.tanistra@gmail.com>
2016-02-04 21:36:14 +0100

    split base to base and selenium base

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
+SET -e
 
 TAG=$1
-for IMAGE_NAME in base hub
+for IMAGE_NAME in base selenium-base selenium-hub
 do
-   IMAGE=atanistra/selenium-$IMAGE_NAME
+   IMAGE=atanistra/$IMAGE_NAME
    docker build -t $IMAGE -f ${IMAGE_NAME}/Dockerfile .
    ID="$(docker images | grep $IMAGE | head -n 1 | awk '{print $3}')"
 	docker tag "$ID" $IMAGE:${TAG}
